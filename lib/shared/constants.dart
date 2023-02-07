94e865380e9a7978e7b0825afff51bd4a466ed59
Abraar Reinhardt <reinhardt.codes@gmail.com>
2023-02-07 14:14:12 +0600

    web auth

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static String appId = "1:691541525607:web:ccfb5e5193a02b499fc0c2";
+  static String apiKey = "AIzaSyBoaztLTRyoJy_b_RWaQs6iXNpe522dv4c";
+  static String messagingSenderId = "691541525607";
+  static String projectId = "nevermissaclass-92aac";
+
+  static String authDomain = "nevermissaclass-92aac.firebaseapp.com";
+  static String storageBucket = "nevermissaclass-92aac.appspot.com";
+}
